9d0cf20b98b7272c2859772fc2d6ad3d5e6dd88e
Bartosz Hejman <bartoszhejman@gmail.com>
2014-09-18 19:10:21 +0200

    Add seed data for spots and users

---
+++ db/fixtures/spot_activities.rb
@@ -0,0 +1,8 @@
+SpotActivity.seed_once(:id,
+  { id: 1, spot_id: 1, activity_id: 1 },
+  { id: 2, spot_id: 1, activity_id: 2 },
+  { id: 3, spot_id: 2, activity_id: 1 },
+  { id: 4, spot_id: 3, activity_id: 1 },
+  { id: 5, spot_id: 3, activity_id: 3 },
+  { id: 6, spot_id: 3, activity_id: 4 }
+)
---
+++ db/fixtures/spots.rb
@@ -0,0 +1,38 @@
+Spot.seed do |s|
+  s.id = 1
+  s.name    = 'Park Agrykola'
+  s.latitude = 52.218138
+  s.longitude  = 21.034430
+  s.short_description  = 'Jeden z ładniejszych parków w Warszawie'
+  s.address1 = ''
+  s.address2  = ''
+  s.post_code  = '00-459'
+  s.city = 'Warszawa'
+  s.country = 'Polska'
+end
+
+Spot.seed do |s|
+  s.id = 2
+  s.name    = 'Tennis club Wola'
+  s.latitude = 52.234116
+  s.longitude  = 20.953875
+  s.short_description  = 'Bardzo przyjemny club'
+  s.address1 = 'Al. Prymasa Tysiąclecia 103'
+  s.address2  = 'park im. Edwarda Szymańskiego'
+  s.post_code  = '01-424'
+  s.city = 'Warszawa'
+  s.country = 'Polska'
+end
+
+Spot.seed do |s|
+  s.id = 3
+  s.name    = 'Park osiedlowy Bielany III'
+  s.latitude = 52.292422
+  s.longitude  = 20.970772
+  s.short_description  = 'Idealny park do wypoczynku ze znajomymi'
+  s.address1 = '12 T. Duracza'
+  s.address2  = ''
+  s.post_code  = '01-200'
+  s.city = 'Warszawa'
+  s.country = 'Polska'
+end
