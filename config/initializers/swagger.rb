0c4287ec9702343b99d12b86b827a03f4996eebc
Adrian Serafin <abusiek@gmail.com>
2014-09-18 12:40:07 +0200

    setup swagger again

---
+++ config/initializers/swagger.rb
@@ -0,0 +1,3 @@
+GrapeSwaggerRails.options.url      = '/api/swagger_doc.json'
+GrapeSwaggerRails.options.app_url  = 'http://inspottle.herokuapp.com'
+GrapeSwaggerRails.options.app_name = 'Inspotle API'
